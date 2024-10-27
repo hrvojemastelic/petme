968088e6317f0967feed6e4a40316c138c9758fc
hrvojemastelic <hrvojemastelic92@gmail.com>
2024-10-27 17:41:29 +0100

    ADD AD  FIXED IMAGE SELECT FIXED PROGRESSBAR

--- app/src/main/java/com/example/petme/ui/ads/addad/AddAdActivity.kt
+++ app/src/main/java/com/example/petme/ui/ads/addad/AddAdActivity.kt
@@ -1,19 +1,29 @@
 package com.example.petme.ui.ads.addad
 
-
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petme.databinding.ActivityAddAdBinding
 import com.example.petme.models.ClassifiedAd
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 import java.util.*
-import kotlin.math.log
 
 class AddAdActivity : AppCompatActivity() {
 
@@ -21,7 +31,7 @@ class AddAdActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
     private lateinit var storage: FirebaseStorage
-    private var selectedImages: List<Uri> = listOf()
+    private val selectedImages: MutableList<Uri> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,92 +42,123 @@ class AddAdActivity : AppCompatActivity() {
         firestore = FirebaseFirestore.getInstance()
         storage = FirebaseStorage.getInstance()
 
-        // Upload image button click event
         binding.btnUploadImages.setOnClickListener {
             openImageSelector()
         }
 
-        // Save Ad button
         binding.btnSaveAd.setOnClickListener {
-            val title = binding.etTitle.text.toString().trim()
-            val description = binding.etDescription.text.toString().trim()
-            val priceText = binding.etPrice.text.toString().trim()
-            val ageText = binding.etAge.text.toString().trim()
-            val category = binding.spinnerCategory.selectedItem.toString()
-            val breed = binding.etBreed.text.toString().trim()
-
-            // Check if all fields are filled
-            if (title.isEmpty()) {
-                Toast.makeText(this, "Please enter a title", Toast.LENGTH_SHORT).show()
-            } else if (description.isEmpty()) {
-                Toast.makeText(this, "Please enter a description", Toast.LENGTH_SHORT).show()
-            } else if (priceText.isEmpty()) {
-                Toast.makeText(this, "Please enter a price", Toast.LENGTH_SHORT).show()
-            } else if (ageText.isEmpty()) {
-                Toast.makeText(this, "Please enter age", Toast.LENGTH_SHORT).show()
-            } else if (category.isEmpty() || category == "Select Category") {
-                Toast.makeText(this, "Please select a category", Toast.LENGTH_SHORT).show()
-            } else if (breed.isEmpty()) {
-                Toast.makeText(this, "Please enter breed", Toast.LENGTH_SHORT).show()
-            } else if (selectedImages.isEmpty()) {
-                Toast.makeText(this, "Please select at least one image", Toast.LENGTH_SHORT).show()
-            } else {
-                // Convert price and age to respective data types
-                val price = priceText.toDoubleOrNull()
-                val age = ageText.toIntOrNull()
-
-                if (price == null) {
-                    Toast.makeText(this, "Invalid price format", Toast.LENGTH_SHORT).show()
-                } else if (age == null) {
-                    Toast.makeText(this, "Invalid age format", Toast.LENGTH_SHORT).show()
-                } else {
-                    // Show the progress bar and proceed to upload images and save ad
-                    binding.progressBar.visibility = android.view.View.VISIBLE
-                    uploadImagesAndSaveAd(title, description, price, age, category, breed)
-                }
-            }
+            saveAd()
         }
     }
-        private fun openImageSelector() {
-        // Intent for selecting multiple images
-        val intent = Intent().apply {
+
+    private fun openImageSelector() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
             type = "image/*"
             putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-            action = Intent.ACTION_GET_CONTENT
         }
         startActivityForResult(Intent.createChooser(intent, "Select Images"), IMAGE_PICK_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == IMAGE_PICK_CODE && resultCode == RESULT_OK) {
+        if (requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK) {
+            // Do not clear the list; instead, keep previously selected images
             if (data?.clipData != null) {
-                // Multiple images selected
-                val count = data.clipData?.itemCount ?: 0
-                val images = mutableListOf<Uri>()
+                val count = data.clipData!!.itemCount
                 for (i in 0 until count) {
-                    val imageUri = data.clipData?.getItemAt(i)?.uri
-                    if (imageUri != null) {
-                        images.add(imageUri)
+                    val imageUri = data.clipData!!.getItemAt(i).uri
+                    if (!selectedImages.contains(imageUri)) {  // Avoid duplicates
+                        selectedImages.add(imageUri)
+                        addImageToContainer(imageUri)
                     }
                 }
-                selectedImages = images
-                Toast.makeText(this, "$count images selected", Toast.LENGTH_SHORT).show()
             } else if (data?.data != null) {
-                // Single image selected
-                val imageUri: Uri = data.data!!
-                selectedImages = listOf(imageUri)
-                Toast.makeText(this, "1 image selected", Toast.LENGTH_SHORT).show()
+                val imageUri = data.data!!
+                if (!selectedImages.contains(imageUri)) {  // Avoid duplicates
+                    selectedImages.add(imageUri)
+                    addImageToContainer(imageUri)
+                }
+            }
+
+            Toast.makeText(this, "${selectedImages.size} images selected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun addImageToContainer(imageUri: Uri) {
+        val imageView = ImageView(this).apply {
+            layoutParams = GridLayout.LayoutParams().apply {
+                width = 200 // Adjust this size as needed for your layout
+                height = 200 // Adjust this size as needed for your layout
+                setMargins(8, 8, 8, 8)
+            }
+            setImageURI(imageUri)
+            scaleType = ImageView.ScaleType.CENTER_CROP
+            setPadding(4, 4, 4, 4)
+            setOnClickListener {
+                // Show confirmation dialog before deleting
+                AlertDialog.Builder(this@AddAdActivity)
+                    .setTitle("Delete Image")
+                    .setMessage("Are you sure you want to delete this image?")
+                    .setPositiveButton("Yes") { _, _ ->
+                        selectedImages.remove(imageUri)
+                        binding.selectedImagesContainer.removeView(this)
+                        Toast.makeText(this@AddAdActivity, "Image removed", Toast.LENGTH_SHORT).show()
+                    }
+                    .setNegativeButton("No") { dialog, _ ->
+                        dialog.dismiss()
+                    }.show()
             }
         }
+        binding.selectedImagesContainer.addView(imageView)
+    }
+
+
+
+    private fun saveAd() {
+        binding.btnSaveAd.isEnabled = false
+        binding.mainContentLayout.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
+
+        val title = binding.etTitle.text.toString().trim()
+        val description = binding.etDescription.text.toString().trim()
+        val priceText = binding.etPrice.text.toString().trim()
+        val ageText = binding.etAge.text.toString().trim()
+        val category = binding.spinnerCategory.selectedItem.toString()
+        val breed = binding.etBreed.text.toString().trim()
+
+        if (title.isEmpty() || description.isEmpty() || priceText.isEmpty() ||
+            ageText.isEmpty() || category == "Select Category" || breed.isEmpty() || selectedImages.isEmpty()
+        ) {
+            binding.btnSaveAd.isEnabled = true
+            binding.progressBar.visibility = View.GONE
+            binding.mainContentLayout.visibility = View.VISIBLE
+
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val price = priceText.toDoubleOrNull()
+        val age = ageText.toIntOrNull()
+
+        if (price == null || age == null) {
+            binding.btnSaveAd.isEnabled = true
+            binding.progressBar.visibility = View.GONE
+            binding.mainContentLayout.visibility = View.VISIBLE
+
+            Toast.makeText(this, "Invalid price or age format", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        binding.progressBar.visibility = View.VISIBLE
+        binding.mainContentLayout.visibility = View.GONE
+
+        uploadImagesAndSaveAd(title, description, price, age, category, breed)
     }
 
     private fun uploadImagesAndSaveAd(title: String, description: String, price: Double, age: Int, category: String, breed: String) {
         val user = auth.currentUser
         if (user != null && selectedImages.isNotEmpty()) {
-            val storageRef = storage.reference
             val imageUrls = mutableListOf<String>()
-
             uploadImage(0, imageUrls, title, description, price, age, category, breed)
         }
     }
@@ -134,37 +175,46 @@ class AddAdActivity : AppCompatActivity() {
     ) {
         if (index < selectedImages.size) {
             val uri = selectedImages[index]
-            val fileName = UUID.randomUUID().toString()
-            val imageRef = storage.reference.child("images/$fileName.jpg")
+            val imageRef = storage.reference.child("images/${UUID.randomUUID()}.jpg")
 
-            val user = auth.currentUser
-            if (user != null) {
-                Log.d("UploadImage", "Uploading image $index: $uri")
-                imageRef.putFile(uri)
+            compressImage(uri)?.let { compressedData ->
+                imageRef.putBytes(compressedData)
                     .addOnSuccessListener {
-                        Log.d("UploadImage", "Image $index uploaded successfully")
                         imageRef.downloadUrl.addOnSuccessListener { downloadUri ->
-                            Log.d("UploadImage", "Image $index download URL: $downloadUri")
                             imageUrls.add(downloadUri.toString())
                             uploadImage(index + 1, imageUrls, title, description, price, age, category, breed)
                         }
                     }
-                    .addOnFailureListener { exception ->
-                        Log.e("UploadImage", "Error uploading image $index: ${exception.message}")
-                        Toast.makeText(this, "Error uploading image at index $index", Toast.LENGTH_SHORT).show()
-                        binding.progressBar.visibility = android.view.View.GONE // Hide on failure
+                    .addOnFailureListener {
+                        binding.btnSaveAd.isEnabled = true
+                        binding.progressBar.visibility = View.GONE
+                        binding.mainContentLayout.visibility = View.VISIBLE
+
+                        Toast.makeText(this, "Error uploading image", Toast.LENGTH_SHORT).show()
                     }
-            } else {
-                Log.e("UploadImage", "User not authenticated")
-                Toast.makeText(this, "Please sign in to upload images", Toast.LENGTH_SHORT).show()
-                binding.progressBar.visibility = android.view.View.GONE // Hide if user is not signed in
+            } ?: run {
+                Toast.makeText(this, "Error processing image", Toast.LENGTH_SHORT).show()
             }
         } else {
-            Log.d("UploadImage", "All images uploaded successfully")
             saveAdToFirestore(title, description, price, age, category, breed, imageUrls)
         }
     }
 
+    private fun compressImage(uri: Uri): ByteArray? {
+        return try {
+            val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, uri))
+            } else {
+                MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            }
+
+            val outputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+            outputStream.toByteArray()
+        } catch (e: IOException) {
+            null
+        }
+    }
 
     private fun saveAdToFirestore(
         title: String,
@@ -177,11 +227,9 @@ class AddAdActivity : AppCompatActivity() {
     ) {
         val user = auth.currentUser
         if (user != null) {
-            // Generate a unique ID for the ad
-            val adId = UUID.randomUUID().toString() // Unique ID for the ad
-
+            val adId = UUID.randomUUID().toString()
             val ad = hashMapOf(
-                "id" to adId, // Include the generated ID
+                "id" to adId,
                 "title" to title,
                 "description" to description,
                 "price" to price,
@@ -189,28 +237,31 @@ class AddAdActivity : AppCompatActivity() {
                 "category" to category,
                 "breed" to breed,
                 "imageUrls" to imageUrls,
-                "userId" to user.uid, // Link ad to current user by user ID
+                "userId" to user.uid,
                 "email" to user.email,
                 "dateCreated" to Date()
             )
 
             firestore.collection("ads")
-                .document(adId) // Use the custom ID to store the ad
-                .set(ad) // Use .set() instead of .add()
+                .document(adId)
+                .set(ad)
                 .addOnSuccessListener {
+                    binding.btnSaveAd.isEnabled = true
+                    binding.progressBar.visibility = View.GONE
+                    binding.mainContentLayout.visibility = View.VISIBLE
+
                     Toast.makeText(this, "Ad created successfully", Toast.LENGTH_SHORT).show()
-                    binding.progressBar.visibility = android.view.View.GONE // Hide on success
-                    finish() // Optionally close the activity after saving
+                    finish()
                 }
-                .addOnFailureListener { exception ->
-                    Log.e("FirestoreError", "Error creating ad: ${exception.message}")
-                    Toast.makeText(this, "Error creating ad: ${exception.message}", Toast.LENGTH_SHORT).show()
-                    binding.progressBar.visibility = android.view.View.GONE // Hide on failure
+                .addOnFailureListener {
+                    binding.btnSaveAd.isEnabled = true
+                    binding.progressBar.visibility = View.GONE
+                    binding.mainContentLayout.visibility = View.VISIBLE
+
+                    Toast.makeText(this, "Error creating ad", Toast.LENGTH_SHORT).show()
                 }
         }
     }
-
-
 
     companion object {
         private const val IMAGE_PICK_CODE = 1000
